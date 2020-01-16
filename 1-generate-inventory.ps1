a2cab550c82d469fd2ced6e6eb9b3d4e74dd7fd2
Rodrigo Santos <20050577+rodrigosantosms@users.noreply.github.com>
2020-01-15 17:49:26 -0800

    updates

--- 1-generate-inventory.ps1
+++ 1-generate-inventory.ps1
@@ -1,15 +1,18 @@
 <#
  	.DISCLAIMER
-    This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.
-    THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
-    INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  
-    We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object
-    code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to market Your software
-    product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the
-    Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims
-    or lawsuits, including attorneys� fees, that arise or result from the use or distribution of the Sample Code.
-    Please note: None of the conditions outlined in the disclaimer above will supersede the terms and conditions contained
-    within the Premier Customer Services Description.
+    MIT License
+
+    Copyright (c) 2020 Rodrigo Santos
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #>
 clear-host
 
@@ -119,7 +122,7 @@ param(
 # 3 - Starting Data Collector
 #
     write-host ""
-    write-host "************************************ COLLECTING DATA FROM TENANT *******************************************"
+    write-host "************************************ COLLECTING DATA FROM SUBSCRIPTION *******************************************"
     write-host ""
     write-host "Source Tenant: " $SourceTenantID " - Subscription: " $sub.Name
     write-host ""
@@ -155,7 +158,7 @@ param(
     Disable-AzContextAutosave  | Out-Null
 
     write-host ""
-    write-host "************************************ DATA SUCCESSFULLY COLLECTED FROM ALL SUBSCRIPTIONS AND TENANT *******************************************"
+    write-host "************************************ DATA SUCCESSFULLY COLLECTED FROM SUBSCRIPTION *******************************************"
     write-host ""
 
 ################################################################################################
